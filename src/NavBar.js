2df049f348f780ca056cce0e14727483fc306a6d
Mustafa Aljuaidi <mustafa.aljuaidi@gmail.com>
2020-07-11 12:30:00 +0400

    NavBar updated

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,17 +4,23 @@ import Button from './Button';
 import logo from './logo.svg';
 import Jumbotron from './Jumbotron';
 
-
 const NavBar = () => {
+
+
     return (
         <nav className="navbar navbar-dark bg-dark">
-            <Link className="navbar-brand" href="#">
-                <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="" loading="lazy"/>
+            <Link className="navbar-brand" to="/">
+                <img src={logo} width="30" height="30" 
+                className="d-inline-block align-top" 
+                alt="" 
+                loading="lazy"/>
                 Bootstrap
             </Link>
+
             <div style={{display: 'flex'}}>
-                <Link 
-                 className="btn btn-primary">
+                <Link
+                to="/login"
+                className="btn btn-primary">
                     Log In
                 </Link>
             </div>
