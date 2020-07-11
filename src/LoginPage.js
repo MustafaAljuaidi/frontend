304d771f565a0dd4736a93c599e520f42a9a18b3
Mustafa Aljuaidi <mustafa.aljuaidi@gmail.com>
2020-07-11 14:04:45 +0400

    updated login page

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -2,6 +2,13 @@ import React from 'react';
 import NavBar from './NavBar.js';
 
 const LoginPage = () => {
+
+    let emailField, passwordField;
+
+    const loginUser = () => {
+        console.log(emailField.value, passwordField.value);
+    }
+
     return(
         <div>
              <NavBar />
@@ -11,30 +18,34 @@ const LoginPage = () => {
                 <div className="row">
                     <div className="col-sm" 
                     style={{maxWidth: '400px', margin: '0 auto'}}>
-                        <form>
+                        <div>
                             <div className="form-group">
                                 <label for="exampleInputEmail1">
                                     Email address
                                 </label>
-                                <input type="email" 
+
+                                <input 
+                                ref={(comp)=> emailField = comp}
+                                type="email" 
                                 className="form-control" 
                                 id="exampleInputEmail1" aria-describedby="emailHelp"/>
                                 <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                             </div>
                             <div className="form-group">
                                 <label for="exampleInputPassword1">Password</label>
-                                <input type="password" 
+
+                                <input
+                                ref={(comp)=> passwordField = comp} 
+                                type="password" 
                                 className="form-control" 
                                 id="exampleInputPassword1"/>
                             </div>
-                            <div className="form-group form-check">
-                                <input type="checkbox" 
-                                className="form-check-input" 
-                                id="exampleCheck1"/>
-                                <label className="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                            <button type="submit" className="btn btn-primary">Submit</button>
-                    </form>
+
+                            <button 
+                            onClick={loginUser}
+                            type="button"
+                            className="btn btn-primary">Login</button>
+                    </div>
                 </div>
             </div>
           </div>
